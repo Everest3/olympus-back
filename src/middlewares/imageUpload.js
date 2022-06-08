58744df572c6039ec2bb26ef09ed215fc0f8dd20
Everest3 <restigjidia@outlook.com>
2022-06-09 00:25:30 +0200

    stored images at folder

--- src/middlewares/imageUpload.js
+++ src/middlewares/imageUpload.js
@@ -1,15 +1,18 @@
 const multer = require('multer');
 
-const imageUpload = multer({
-  limits: {
-    fileSize: 500000
+const fileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/')
   },
-  fileFilter(req, file, cb) {
+  filename: function (req, file, cb) {
     if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
       return cb(new Error("Please upload an image"))
     }
-    cb(undefined, true)
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)+'.'+file.originalname.split('.').pop();
+    cb(null, file.fieldname + '-' + uniqueSuffix)
   }
 })
+
+const imageUpload=multer({storage:fileStorage})
 
 module.exports = imageUpload;
